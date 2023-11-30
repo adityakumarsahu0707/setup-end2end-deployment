a9b2685c70e1f7af751c12c7ab6aa2226fe3dc12
adityakumarsahu0707 <adityakumarsahu45@gmail.com>
2023-11-30 23:47:20 +0530

    Changes added

--- code-ws/index.php
+++ code-ws/index.php
@@ -1,5 +1,15 @@
-<?php
-$ifconfig=`ifconfig`;
-echo("<pre> {$ifconfig} </pre>");
-
-?>
+<!DOCTYPE html> 
+<html> 
+<head> 
+	<title>PHP</title> 
+</head> 
+<body> 
+	<h1> <pre>
+		<?php 
+			$ip=shell_exec('/usr/sbin/ifconfig');
+			print $ip;
+			echo "hii GeeksforGeeks"
+		?> 
+</pre>	</h1> 
+</body> 
+</html>
